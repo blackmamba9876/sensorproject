1ab2ee9bae232e645c28e1d46a58e229ddea9e9a
blackmamba9876 <suhasrao1218@gmail.com>
2024-09-14 13:51:36 +0530

    14th commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,7 +2,7 @@ import sys
 import os
 import numpy as np
 import pandas as pd
-import pymongo import MongoClient 
+from  pymongo import MongoClient 
 from zipfile import Path
 from src.constant import *
 from src.exception import CustomException
